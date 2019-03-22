d3f0f6140c79ccf9bd46ed63b13516956f4ca5f4
Aisa <aisajemila@gmail.com>
2019-03-22 18:01:02 +0300

    Created a quote-board component.

---
+++ src/app/quote-board/quote-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-board',
+  templateUrl: './quote-board.component.html',
+  styleUrls: ['./quote-board.component.css']
+})
+export class QuoteBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
