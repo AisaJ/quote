028577a717aae681ebd5b4db7d310813bffb8eef
Aisa <aisajemila@gmail.com>
2019-03-22 17:55:32 +0300

    Created a constructor for the class and array list for the quotes. generated a class.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote  } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes';
+    
+    quotes = [
+      new Quote(1,"Mind over matter"),
+      new Quote(2,"Not all counted truly counts"),
+      new Quote(3,"Not all that counts is counted"),
+    ]
+   
 }
